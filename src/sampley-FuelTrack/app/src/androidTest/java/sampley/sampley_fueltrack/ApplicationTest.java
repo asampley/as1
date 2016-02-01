e28198b3d36dfcf96ea7107f0b1952083afd649e
Alex <alex.sampley@gmail.com>
2016-02-01 15:09:40 -0700

    Added more tests

--- src/sampley-FuelTrack/app/src/androidTest/java/sampley/sampley_fueltrack/ApplicationTest.java
+++ src/sampley-FuelTrack/app/src/androidTest/java/sampley/sampley_fueltrack/ApplicationTest.java
@@ -6,6 +6,8 @@ import android.test.ApplicationTestCase;
 import java.util.Date;
 
 import sampley.sampley_fueltrack.data.Entry;
+import sampley.sampley_fueltrack.data.Fuel;
+import sampley.sampley_fueltrack.data.Station;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -25,5 +27,15 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         assertTrue(entry.getFuel().getUnitCost() == 99.3f);
         assertTrue(entry.getFuel().getAmount() == 45.234f);
         assertTrue(entry.getOdometer() == 100248.3f);
+
+        Date now2 = new Date();
+        Entry entry2 = new Entry(now, new Station("Esso"), new Fuel("extra", 88.3f, 12.234f), 100200.3f);
+
+        assertTrue(entry2.getDate().equals(now));
+        assertTrue(entry2.getStation().toString().equals("Esso"));
+        assertTrue(entry2.getFuel().getGrade().equals("extra"));
+        assertTrue(entry2.getFuel().getUnitCost() == 88.3f);
+        assertTrue(entry2.getFuel().getAmount() == 12.234f);
+        assertTrue(entry2.getOdometer() == 100200.3f);
     }
 }
